bceee8fc3f1d17853767c5c8cab04d9e6aa5464a
crespire <li.simmon@gmail.com>
2022-03-03 12:49:54 -0500

    Update Kittens controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -5,19 +5,23 @@ class KittensController < ApplicationController
   def show
   end
 
+  # Input form to create model instance.
   def new
   end
 
+  # Save model instance to DB.
   def create
   end
 
+  # Input form to edit model instance.
   def edit
   end
 
+  # Save changes to model instance to DB.
   def update
   end
 
-  def delete
+  def destroy
   end
 end
 
