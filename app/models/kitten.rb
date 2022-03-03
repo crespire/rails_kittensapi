f2cfee91497449c5f1ed6b3c72ec05aab185ba4e
crespire <li.simmon@gmail.com>
2022-03-03 13:09:48 -0500

    Add some validations to model

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,9 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :cuteness, presence: true
+  validates :softness, presence: true
+  validates :cuteness, inclusion: { in: 1..5 }
+  validates :softness, inclusion: { in: 1..5 }
+
 end
