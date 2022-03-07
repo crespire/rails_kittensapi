abbe4d8d39907bb95ce820be0dc91d284ea97a91
crespire <li.simmon@gmail.com>
2022-03-07 10:47:27 -0500

    Finish Kitten controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -13,6 +13,13 @@ class KittensController < ApplicationController
 
   # Save model instance to DB.
   def create
+    @kitten = Kitten.new(kitten_params)
+
+    if @kitten.save
+      redirect_to kittens_path, notice: 'Kitten created!'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   # Input form to edit model instance.
@@ -21,9 +28,18 @@ class KittensController < ApplicationController
 
   # Save changes to model instance to DB.
   def update
+    if @kitten.update(kitten_params)
+      redirect_to kitten_url(@kitten), notice: 'Kitten updated!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @kitten = Kitten.find(params[:id])
+    @kitten.destroy
+
+    redirect_to :root, notice: 'Kitten destroyed.'
   end
 
   private
